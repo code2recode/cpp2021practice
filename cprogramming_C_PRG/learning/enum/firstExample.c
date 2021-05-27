7b6f72a0b2d259048d29004d57eb9e6e1ed1c2a6
iamhaider07 <heatherlinux@gmail.com>
2021-05-27 21:52:49 +0530

    First C Example

---
+++ cprogramming_C_PRG/learning/enum/firstExample.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*
+enum resut {pass, fail};
+    enum result s1,s2;
+    s1=pass;
+    s2=fail;
+    printf("%d",s1);
+	
+*/	
+enum SEX{
+Male=0, Female
+};
+
+int main()
+{
+  SEX var = Male;
+  printf("%d",var);
+  return 0;
+}
